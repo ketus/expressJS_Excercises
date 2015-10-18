d1e88e4684ca82fea8379d605100c4c470fa1f36
Ketus <bardzo.mlody@gmail.com>
2015-10-18 19:58:47 +0000

    Complete "Static"

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
- var express = require('express')
-    var app = express()
-    app.get('/home', function(req, res) {
-      res.end('Hello World!\n')
-    })
-    app.listen(process.argv[2])
+ var express = require('express');
+var app = express();
+    
+app.use(express.static(process.argv[3]));
+    
+app.listen(process.argv[2]);
